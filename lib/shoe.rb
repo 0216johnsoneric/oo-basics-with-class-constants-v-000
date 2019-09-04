2c0a92cdc2b0810d06446d45ca213a89ea7c5f69
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-04 00:56:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,23 +1,23 @@
-# class Shoe
-#   attr_accessor :color, :size, :material
-#   attr_reader :brand, :condition
-#
-#
-#   BRANDS = []
-#
-#   def initialize(brand)
-#     @brand = brand
-#     BRANDS << brand unless BRANDS.any? { |b| b == brand }
-#   end
-#
-#   def cobble
-#     self.condition = "new"
-#     puts "Your shoe is as good as new!"
-#   end
-#
-#   def brand=(brand)
-#     @brand = brand
-#     BRANDS << brand
-#   end
-#
-# end
+class Shoe
+  attr_accessor :color, :size, :material
+  attr_reader :brand, :condition
+
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.any? { |b| b == brand }
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+
+end
